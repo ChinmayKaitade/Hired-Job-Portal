f8771684efbc1d68abb017a30ce353850a40bf40
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-09-02 14:54:14 +0530

    Shadcn UI Installation and UI components added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css"
+import { Button } from "./components/ui/button"
+
+function App() {
+  return (
+    <div>
+      <Button>Login</Button>
+    </div>
+  )
+}
+
+export default App
