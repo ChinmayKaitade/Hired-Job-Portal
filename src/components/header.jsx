56072fd9a50c5bb8eb8a84d02305abca57b2ceff
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-09-03 16:45:55 +0530

    Profile or User account section updated

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,13 +1,27 @@
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { Button } from "./ui/button";
-// import {
-//   SignedIn,
-//   SignedOut,
-//   SignInButton,
-//   UserButton,
-// } from "@clerk/clerk-react";
+import { SignedIn, SignedOut, SignIn, UserButton } from "@clerk/clerk-react";
+import { BriefcaseBusiness, Heart, PenBox } from "lucide-react";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+  const [ShowSignIn, setShowSignIn] = useState(false);
+
+  const [search, setSearch] = useSearchParams();
+
+  useEffect(() => {
+    if (search.get("sign-in")) {
+      setShowSignIn(true);
+    }
+  }, [search]);
+
+  const handleOverlayClick = (e) => {
+    if (e.target === e.currentTarget) {
+      setShowSignIn(false);
+      setSearch({});
+    }
+  };
+
   return (
     <>
       <nav className="py-4 flex justify-between items-center">
@@ -16,17 +30,60 @@ const Header = () => {
           <img src="/logo.png" alt="logo" className="h-20" />
         </Link>
 
-        {/* login button */}
-        <Button variant="outline">Login</Button>
-
         {/* Clerk SignOut Buttons */}
-        {/* <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn> */}
+        <div className="flex gap-8">
+          <SignedOut>
+            {/* login button */}
+            <Button variant="outline" onClick={() => setShowSignIn(true)}>
+              Login
+            </Button>
+          </SignedOut>
+
+          <SignedIn>
+            {/* post job button */}
+            <Button variant="destructive" className="rounded-full">
+              Post a Job
+              <PenBox size={20} className="mr-2" />
+            </Button>
+            {/* condition for posting job as recruiter */}
+            <Link to="/post-job"></Link>
+            <UserButton
+              appearance={{
+                elements: {
+                  avatarBox: "w-10 h-10",
+                },
+              }}
+            >
+              <UserButton.MenuItems>
+                <UserButton.Link
+                  label="My Jobs"
+                  labelIcon={<BriefcaseBusiness size={15} />}
+                  href="/my-jobs"
+                />
+                <UserButton.Link
+                  label="Saved Jobs"
+                  labelIcon={<Heart size={15} />}
+                  href="/saved-jobs"
+                />
+                <UserButton.Action label="manageAccount" />
+              </UserButton.MenuItems>
+            </UserButton>
+          </SignedIn>
+        </div>
       </nav>
+
+      {/* SignIn and SignUp Card */}
+      {ShowSignIn && (
+        <div
+          className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
+          onClick={handleOverlayClick}
+        >
+          <SignIn
+            signUpForceRedirectUrl="/onboarding"
+            fallbackRedirectUrl="/onboarding"
+          />
+        </div>
+      )}
     </>
   );
 };
