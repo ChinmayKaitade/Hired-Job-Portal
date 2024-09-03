d627ba8c05201476b0934b31e250eb71adc58d87
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-09-03 08:23:49 +0530

    Supabase with Clerk Connection Setup done

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -2,6 +2,17 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
 
-export default supabase;
+const supabaseClient = async (supabaseAccessToken) => {
+  const supabase = createClient(supabaseUrl, supabaseKey, {
+    global: {
+      headers: {
+        Authorization: `Bearer ${supabaseAccessToken}`,
+      },
+    },
+  });
+
+  return supabase;
+};
+
+export default supabaseClient;
