bcd478c58c644d3917007a28d3722dfe0254c77b
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-09-03 19:45:41 +0530

    Protected Route added for authentication purpose via clerk

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import PostJob from "./pages/post-job";
 import SavedJobs from "./pages/saved-job";
 import MyJobs from "./pages/my-jobs";
 import { ThemeProvider } from "./components/theme-provider";
+import ProtectedRoute from "./components/protected-route";
 
 const router = createBrowserRouter([
   {
@@ -20,27 +21,51 @@ const router = createBrowserRouter([
       },
       {
         path: "/onboarding",
-        element: <Onboarding />,
+        element: (
+          <ProtectedRoute>
+            <Onboarding />
+          </ProtectedRoute>
+        ),
       },
       {
         path: "/jobs",
-        element: <JobListing />,
+        element: (
+          <ProtectedRoute>
+            <JobListing />
+          </ProtectedRoute>
+        ),
       },
       {
         path: "/job/:id",
-        element: <JobPage />,
+        element: (
+          <ProtectedRoute>
+            <JobPage />
+          </ProtectedRoute>
+        ),
       },
       {
         path: "/post-job",
-        element: <PostJob />,
+        element: (
+          <ProtectedRoute>
+            <PostJob />
+          </ProtectedRoute>
+        ),
       },
       {
-        path: "/saved-job",
-        element: <SavedJobs />,
+        path: "/saved-jobs",
+        element: (
+          <ProtectedRoute>
+            <SavedJobs />
+          </ProtectedRoute>
+        ),
       },
       {
         path: "/my-jobs",
-        element: <MyJobs />,
+        element: (
+          <ProtectedRoute>
+            <MyJobs />
+          </ProtectedRoute>
+        ),
       },
     ],
   },
---
+++ src/components/protected-route.jsx
@@ -0,0 +1,15 @@
+import { useUser } from "@clerk/clerk-react";
+import { Navigate, useLocation } from "react-router-dom";
+
+const ProtectedRoute = ({ children }) => {
+  const { isSignedIn, user, isLoaded } = useUser();
+  const { pathname } = useLocation();
+
+  if (isLoaded && !isSignedIn && isSignedIn !== undefined) {
+    return <Navigate to="/?sign-in=true" />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
