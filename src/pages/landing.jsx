ed874520708f4b1c50149741f631938bacc9432b
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-09-03 15:34:33 +0530

    Carousel and Banner added

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,5 +1,12 @@
 import { Button } from "@/components/ui/button";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import companies from "../data/companies.json";
 import { Link } from "react-router-dom";
+import Autoplay from "embla-carousel-autoplay";
 
 const LandingPage = () => {
   return (
@@ -27,16 +34,38 @@ const LandingPage = () => {
       <div className="flex gap-6 justify-center">
         {/* buttons */}
         <Link to="/jobs">
-          <Button variant="blue" size="xl">Find Jobs</Button>
+          <Button variant="blue" size="xl">
+            Find Jobs
+          </Button>
         </Link>
 
         <Link to="/post-job">
-          <Button variant="destructive" size="xl">Post a Job</Button>
+          <Button variant="destructive" size="xl">
+            Post a Job
+          </Button>
         </Link>
       </div>
+
       {/* carousel */}
+      <Carousel
+        plugins={[Autoplay({ delay: 2000, stopOnInteraction: true })]}
+        className="w-full py-10"
+      >
+        <CarouselContent className="flex gap-5 sm:gap-20 items-center">
+          {companies.map(({ name, id, path }) => (
+            <CarouselItem key={id} className="basis-1/3 lg:basis-1/6 ">
+              <img
+                src={path}
+                alt={name}
+                className="h-9 sm:h-14 object-contain"
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
 
       {/* banner */}
+      <img src="/banner.jpeg" className="w-full" />
 
       <section>{/* cards */}</section>
 
