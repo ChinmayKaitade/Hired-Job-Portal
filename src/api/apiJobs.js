648e077d97c93e37d9137317341d48090c9c4cb0
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-09-04 19:42:01 +0530

    Fetch Jobs API logic added

---
+++ src/api/apiJobs.js
@@ -0,0 +1,30 @@
+import supabaseClient from "@/utils/supabase";
+
+// Fetch Jobs
+export async function getJobs(token, { location, company_id, searchQuery }) {
+  const supabase = await supabaseClient(token);
+  let query = supabase
+    .from("jobs")
+    .select("*, saved: saved_jobs(id), company: companies(name,logo_url)");
+
+  if (location) {
+    query = query.eq("location", location);
+  }
+
+  if (company_id) {
+    query = query.eq("company_id", company_id);
+  }
+
+  if (searchQuery) {
+    query = query.ilike("title", `%${searchQuery}%`);
+  }
+
+  const { data, error } = await query;
+
+  if (error) {
+    console.error("Error fetching Jobs:", error);
+    return null;
+  }
+
+  return data;
+}
